34f1c06ce28f4956882a65f7486e429d4d05afa8
hsxr <413573531@qq.com>
2023-07-07 18:22:37 +0800

    fix:删除文件

--- node_modules/@volar/typescript/out/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './documentRegistry';
-export * from './dtsHost';
-export * from './languageService';
-export * from './languageServiceHost';
-export * from './sys';
-export * from './getProgram';
-export * from './serverPlugin';
